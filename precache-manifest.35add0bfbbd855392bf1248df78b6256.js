e2813cc1d14d7aa296dba3178e1aa76cff9be553
Shivam Malhotra <shivammalhotra08@gmail.com>
2022-03-11 22:35:48 +0530

    Add files

---
+++ precache-manifest.35add0bfbbd855392bf1248df78b6256.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc536e778f914ce9041cc2a5b18c218e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9942a776beb0faae76ba",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "2bb8a5b35398b4e78471",
+    "url": "./static/js/2.8c397a4c.chunk.js"
+  },
+  {
+    "revision": "330e082d6c7ff80e35712939079a4b57",
+    "url": "./static/js/2.8c397a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757aa337c9ea5e39002f",
+    "url": "./static/js/3.c71c92fc.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "./static/js/3.c71c92fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9942a776beb0faae76ba",
+    "url": "./static/js/main.652c6371.chunk.js"
+  },
+  {
+    "revision": "ae48d65f00f5999958eb",
+    "url": "./static/js/runtime-main.6dc4ec75.js"
+  }
+]);
